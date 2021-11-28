62548b4321fb25385dee35e4928f93eaa79987a8
aroquega <aroquega@gmail.com>
2021-11-28 12:12:39 -0500

    add status codes for the create object and delete object endpoints

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status
 from sqlalchemy.orm import Session
 
 import crud
@@ -25,11 +25,11 @@ def get_objects(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return crud.get_objects(db, skip=skip, limit=limit)
 
 
-@app.post("/objects", response_model=schemas.Object)
+@app.post("/objects", response_model=schemas.Object, status_code=status.HTTP_201_CREATED)
 def create_object(object: schemas.ObjectCreate, db: Session = Depends(get_db)):
     return crud.create_object(db, object=object)
 
 
-@app.delete("/objects/{object_id}")
+@app.delete("/objects/{object_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_object(object_id: int, db: Session = Depends(get_db)):
     crud.delete_object(db, object_id=object_id)
